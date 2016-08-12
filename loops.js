fdd7160d33141ae44179c3f46a57856f5311dbfb
Taylor King <dekkofilms@gmail.com>
2016-08-12 13:13:10 -0500

    loops working

---
+++ loops.js
@@ -0,0 +1,31 @@
+for (var i = 0; i <= 1000; i+=100) {
+  console.log(i);
+}
+
+for (var i = 1; i <= 128; i*=2) {
+  console.log(i);
+}
+
+for (var i = 0; i <= 10; i+=2) {
+  console.log(i);
+}
+
+for (var i = 3; i <= 15; i+=3) {
+  console.log(i);
+}
+
+for (var i = 9; i >= 0; i--) {
+  console.log(i);
+}
+
+for (var i = 1; i < 5; i++) {
+  for (var j = 1; j < 4; j++) {
+     console.log(i);
+  }
+}
+
+for (i = 0; i < 3; i++) {
+  for (var j = 0; j < 5; j++) {
+    console.log(j);
+  }
+}
